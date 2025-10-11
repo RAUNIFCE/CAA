0e4d4e465b28b81920e61399b0d849bec5f1d556
RUANIFCE <ruan.goncalves08@aluno.ifce.edu.br>
2025-10-11 16:17:11 -0300

    Update inserctionsortebuscabinaria.c

--- inserctionsortebuscabinaria.c
+++ inserctionsortebuscabinaria.c
@@ -1,6 +1,16 @@
-
 #include <stdio.h>
-
+int buscabinaria(int vetor[],int chave, int inicio,int fim){
+    int meio;
+    while(inicio<=fim){
+        meio=(inicio+fim)/2;
+        if(chave<vetor[meio]){
+            fim=meio-1;
+        }else{
+            inicio=meio+1;
+        }
+    }
+    return inicio;
+}
 int main()
 {
    /*int chave;
@@ -20,13 +30,21 @@ int main()
         printf("%d ",vetor[i]);
     }
     */
-    int chave;
+    //inserction sort com busca binaria
+    int chave,pos;
     int vetor[5]={5,4,3,2,1};
     for(int i=1;i<5;i++){
         chave=vetor[i];
-        pos=buscabinaria(vetor,chave,1,i-1);
-        for(int j=i-1;j>pos2    )
-        
+        pos=buscabinaria(vetor,chave,0,i-1);
+        printf("\n %d",pos);
+        for(int j=i-1;j>=pos;j--){
+            vetor[j+1]=vetor[j];
+        }
+        vetor[pos]=chave;
     }
+    for(int i=0;i<5;i++){
+            printf("[%d]",vetor[i]);
+        }
+        
     return 0;
 }
